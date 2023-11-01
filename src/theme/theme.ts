169b76d097aee8dcd1fad3e649b4f6c114b2631d
Paul <rayver2012@gmail.com>
2023-11-01 13:51:32 +0200

    * Typography theme and label animation

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -8,15 +8,27 @@ const theme = createTheme({
       textTransform: "none",
     },
     h1: {
+      fontSize: 45,
+      fontWeight: 700,
+    },
+    h2: {
       fontSize: 40,
-      fontWeight: 600,
+      fontWeight: 700,
     },
     h3: {
-      fontSize: 37,
-      fontWeight: 600,
+      fontSize: 36,
+      fontWeight: 700,
+    },
+    h4: {
+      fontSize: 30,
+      fontWeight: 700,
     },
     h5: {
       fontSize: 26,
+      fontWeight: 700,
+    },
+    h6: {
+      fontSize: 24,
       fontWeight: 700,
     },
     body1: {
@@ -28,6 +40,14 @@ const theme = createTheme({
       fontSize: 17,
       fontWeight: 500,
       lineHeight: "21px",
+    },
+    subtitle1: {
+      fontSize: 16,
+      fontWeight: 500,
+    },
+    subtitle2: {
+      fontSize: 14,
+      fontWeight: 400,
     }
   },
 });
