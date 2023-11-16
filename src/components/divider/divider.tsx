d60de8047adf1c169da58a89465e86a61174c64d
Paul <rayver2012@gmail.com>
2023-11-16 13:58:17 +0200

    + Start education page

--- src/components/divider/divider.tsx
+++ src/components/divider/divider.tsx
@@ -3,19 +3,33 @@ import { Typography } from "../typography/typography";
 import { ContentCard } from "../contentCard/contentCard";
 
 import cl from "./divider.module.scss";
+import { getValidClassNames } from "@/helpers";
 
 type Properties = {
   firsRow: string;
   secondRow?: string;
   bgColor: string;
-  id?: string
+  id?: string;
+  width?: string;
+  className?: string;
 };
 
-const Divider: React.FC<Properties> = ({ firsRow, secondRow, bgColor, id }) => {
+const Divider: React.FC<Properties> = ({
+  firsRow,
+  secondRow,
+  bgColor,
+  id,
+  width,
+  className,
+}) => {
   return (
     <div className={cl.wrapper} id={id}>
       <div className={cl.divider}></div>
-      <ContentCard className={cl.card} cardBgColor={bgColor}>
+      <ContentCard
+        className={getValidClassNames(cl.card, className)}
+        cardBgColor={bgColor}
+        width={width}
+      >
         <Typography className={cl.firstRow} variant="body1">
           {firsRow}
         </Typography>
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,4 @@
+export { getSocialIconsLinks } from "./getSocialIconsLinks";
 export { getIconSrc } from "./getIconSrc";
 export { getValidClassNames } from "./getValidClassNames";
 export { getIconArtSrc } from "./getIconArtSrc";
