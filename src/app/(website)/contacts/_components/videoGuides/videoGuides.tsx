f31e5a69e1a51072bdefe53761b8ee806659d804
Paul <rayver2012@gmail.com>
2024-02-03 13:06:17 +0200

    *Fix guides cards and some links

--- src/app/(website)/contacts/_components/videoGuides/videoGuides.tsx
+++ src/app/(website)/contacts/_components/videoGuides/videoGuides.tsx
@@ -1,71 +1,86 @@
 "use client";
 import PlayButtonIcon from "/public/icons/playButton.svg";
-import { ContentCard, Typography, Carousel, CarouselItem } from "@/components";
+import {
+  ContentCard,
+  Typography,
+  Carousel,
+  CarouselItem,
+  DialogGallery,
+} from "@/components";
 import { textContent } from "../../textContent";
-import Link from "next/link";
 import cl from "./videoGuides.module.scss";
 import { useWindowSize } from "@uidotdev/usehooks";
+import { useAppDispatch } from "@/redux/hooks";
+import { openGalleryDialog } from "@/redux/slices/galleryDialog/galleryDialogSlice";
 
 const VideoGuides = () => {
   const { width } = useWindowSize();
+  const dispatch = useAppDispatch();
 
-  return width && width < 600 ? (
-    <Carousel
-      initialSlide={0}
-      slideAmount={textContent.videoGuidesBlock.cards.length}
-      useNumbers
-      dots={false}
-      slidesToShow={1}
-      className={cl.carousel}
-      infinite={false}
-      centerMode
-      id="videoGuides"
-    >
-      {textContent.videoGuidesBlock.cards.map((card, i) => (
-        <CarouselItem key={i}>
-          <Link
-            href={
-              card.href ||
-              "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
-            }
-          >
+  return (
+    <>
+      <DialogGallery />
+      {width && width < 600 ? (
+        <Carousel
+          initialSlide={0}
+          slideAmount={textContent.videoGuidesBlock.cards.length}
+          useNumbers
+          dots={false}
+          slidesToShow={1}
+          className={cl.carousel}
+          infinite={false}
+          centerMode
+          id="videoGuides"
+        >
+          {textContent.videoGuidesBlock.cards.map((card, i) => (
+            <CarouselItem key={i}>
+              <ContentCard
+                width="384px"
+                cardBgColor={card.background}
+                className={cl.videoCard}
+                onClick={() =>
+                  dispatch(
+                    openGalleryDialog({
+                      type: "video",
+                      src: card.href,
+                    })
+                  )
+                }
+              >
+                <div className={cl.icon}>
+                  <PlayButtonIcon style={{ width: "50px", height: "auto" }} />
+                </div>
+                <Typography variant="body2">{card.caption}</Typography>
+              </ContentCard>
+            </CarouselItem>
+          ))}
+        </Carousel>
+      ) : (
+        <div className={cl.videoGuidesBlock} id="videoGuides">
+          {textContent.videoGuidesBlock.cards.map((card, i) => (
             <ContentCard
+              key={i}
               width="384px"
               cardBgColor={card.background}
               className={cl.videoCard}
+              onClick={() =>
+                dispatch(
+                  openGalleryDialog({
+                    type: "video",
+                    src: card.href,
+                  })
+                )
+              }
             >
               <div className={cl.icon}>
-                <PlayButtonIcon style={{ width: "50px", height: "auto" }} />
+                <PlayButtonIcon />
               </div>
               <Typography variant="body2">{card.caption}</Typography>
             </ContentCard>
-          </Link>
-        </CarouselItem>
-      ))}
-    </Carousel>
-  ) : (
-    <div className={cl.videoGuidesBlock} id="videoGuides">
-      {textContent.videoGuidesBlock.cards.map((card, i) => (
-        <Link
-          href={
-            card.href ||
-            "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D"
-          }
-          key={i}
-        >
-          <ContentCard
-            width="384px"
-            cardBgColor={card.background}
-            className={cl.videoCard}
-          >
-            <div className={cl.icon}>
-              <PlayButtonIcon />
-            </div>
-            <Typography variant="body2">{card.caption}</Typography>
-          </ContentCard>
-        </Link>
-      ))}
-    </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
