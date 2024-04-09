2096011f72a866920d2c5ab609d2d8e118dff69f
Paul <rayver2012@gmail.com>
2024-04-09 20:08:30 +0300

    * Error page

--- src/app/(website)/error/page.tsx
+++ src/app/(website)/error/page.tsx
@@ -1,15 +1,16 @@
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "Error | Tanpopo",
-};
-const getData = async () => {
-  const data = "Some data";
-  throw new Error();
-  return data;
-};
+import ErrorPage from "../error";
 
-export default async function Home() {
-  const result = await getData();
-  return <main>Error page: {result}</main>;
+export default function Home() {
+  const error = new Error("Custom error");
+
+  return (
+    <ErrorPage
+      error={error}
+      reset={() => {
+        window.location.reload();
+      }}
+    />
+  );
 }
